7dfbb6b693ae9d09ae4502d083d24289eb917869
EthanJamesLew <ethan.lew@pcc.edu>
2016-02-19 20:13:23 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 			{
 				std::vector<std::string> joined = ppl->getJoined();
 				std::cout << utils::vectorToStr(ppl->getJoined()) << " has joined." << std::endl;
-				std::string command= "python python/sms.py " + utils::vectorToStr(ppl->getJoined()) + " online";
+				std::string command= "python python/sms.py -u " + utils::vectorToStr(ppl->getJoined()) + " -s online";
 				system(command.c_str());
 				for (unsigned i = 0; i < joined.size(); i++)
 				{
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 			{
 				std::vector<std::string> left = ppl->getLeft();
 				std::cout << utils::vectorToStr(ppl->getLeft()) << "has left.";
-				std::string command = "python python/sms.py " + utils::vectorToStr(ppl->getJoined()) + " offline";
+				std::string command = "python python/sms.py -u " + utils::vectorToStr(ppl->getJoined()) + " -s offline";
 				system(command.c_str());
 
 				for (unsigned i = 0; i < left.size(); i++)
